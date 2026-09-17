6fc7855a5ebce18b8b1eb56248f393a695ebff82
Kristal Pithva <kristal.p@mail.enlivendc.com>
2026-09-17 16:06:22 +0530

    refactor: reorganize javascript exercises into subdirectory and add python script

---
+++ python/dayOne.py
@@ -0,0 +1,30 @@
+# name = "kristal"
+# experience = 2
+# hourly_rate = 20
+# is_learning_ai = True
+
+# print("name =>", name)
+# print("experience =>", experience)
+
+# print("type of name =>",type(name))
+# print("type of experience =>" ,type(experience))
+
+
+# ================================================
+
+product_price = 300
+gst = 18
+
+total_gst = product_price*gst/100
+final_price= product_price + total_gst
+
+
+print("Product Price =", product_price)
+print("GST Percentage =", gst,"%")
+print("GST Amount =", total_gst)
+print("Final Price =" ,final_price)
+
+# ================================================
+
+
+price = float(input("Enter product price: "))

c95287887d703c346d3abf475897f9c90612a741
Kristal Pithva <kristal.p@mail.enlivendc.com>
2026-09-17 16:13:15 +0530

    feat: uncomment python variables and add developer profile interactive script

--- python/dayOne.py
+++ python/dayOne.py
@@ -1,16 +1,16 @@
-# name = "kristal"
-# experience = 2
-# hourly_rate = 20
-# is_learning_ai = True
+name = "kristal"
+experience = 2
+hourly_rate = 20
+is_learning_ai = True
 
-# print("name =>", name)
-# print("experience =>", experience)
+print("name =>", name)
+print("experience =>", experience)
 
-# print("type of name =>",type(name))
-# print("type of experience =>" ,type(experience))
+print("type of name =>",type(name))
+print("type of experience =>" ,type(experience))
 
 
-# ================================================
+# ============== GST calculator ===============================
 
 product_price = 300
 gst = 18
@@ -24,7 +24,20 @@ print("GST Percentage =", gst,"%")
 print("GST Amount =", total_gst)
 print("Final Price =" ,final_price)
 
-# ================================================
+# =================== Developer Profile =============================
+
+# Name
+# Years of experience
+# Current technology
+# Target technology
 
 
-price = float(input("Enter product price: "))
+name = input("Enter Name: ")
+experience = int(input("Enter Experience: "))
+current_tech = input("Enter Current Technology: ")
+target_tech = input("Enter Target Technology: ")
+
+print("Name:", name)
+print("Experience:", experience)
+print("Current Technology:", current_tech)
+print("Target Technology:", target_tech)
